de7f7ddf30814b62c6f565269b0e2e1a8bee8f5c
lhuaman-visma <lennin.huaman@visma.com>
2025-09-22 12:19:36 -0500

    Added magic link

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -19,6 +19,9 @@ export class User {
   @Prop({ required: true, unique: true })
   email: string;
 
+  @Prop({ required: true, unique: true})
+  phone: string;
+
   @Prop({ required: true })
   password: string;
 
